9d000ec1b28b56ccdd5f1b99c8c9a1455914a38f
Arturo Jimenez <arturo.jimenez@freejolitos.com>
2024-01-23 23:47:44 -0600

    avance QR

--- components/qr-buzzword/qr-buzzword.controller.js
+++ components/qr-buzzword/qr-buzzword.controller.js
@@ -7,7 +7,8 @@ const Role = require('_helpers/role');
 const qrBuzzwordService = require('./qr-buzzword.service');
 
 // Routes
-router.get('/:id', getEmployeeById);
+router.get('/empleado/:id', getEmployeeById);
+router.get('/test', getQrEjemplo);
 
 
 module.exports = router;
@@ -16,5 +17,11 @@ module.exports = router;
 function getEmployeeById(req, res, next) {
     qrBuzzwordService.getEmployeeById(req.params.id)
         .then(employee => employee ? res.json(employee) : res.sendStatus(404))
+        .catch(next);
+}
+
+function getQrEjemplo(req, res, next) {
+    qrBuzzwordService.qrEjemplo()
+        .then(qrEjemplo => qrEjemplo ? res.json(qrEjemplo) : res.sendStatus(500))
         .catch(next);
 }
--- components/qr-buzzword/qr-buzzword.service.js
+++ components/qr-buzzword/qr-buzzword.service.js
@@ -2,12 +2,24 @@ const config = require('config.json');
 const { Op } = require('sequelize');
 const db = require('_helpers/db');
 const Role = require('_helpers/role');
+const QRCode = require('qrcode');
 
 module.exports = {
-    getEmployeeById
+    getEmployeeById,
+    qrEjemplo
 };
 
 async function getEmployeeById(id) {
     const employee = await db.QrBuzzword.findByPk(id);
     return employee;
+}
+
+async function qrEjemplo() {
+    const qrEjemplo = QRCode.toDataURL('https://freejolitos.com', function (err, url) {
+        console.log(url)
+    });
+    const qrEjemplo2 = QRCode.toString('https://freejolitos.com', { type: 'terminal' }, function (err, url) {
+        console.log(url)
+    });
+    return 1;
 }
